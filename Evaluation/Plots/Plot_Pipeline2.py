949ea36691bc86428dd745b40fbf78e21d00b5c8
Roby46 <r.stanzione9@studenti.unisa.it>
2025-05-21 11:13:27 +0200

    Resolved Conflicts

---
+++ Evaluation/Plots/Plot_Pipeline2.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
+def load_rmse_data(filepath, selected_datasets):
+    df = pd.read_csv(filepath, sep=';')
+    datasets = {}
+
+    for dataset_name in selected_datasets:
+        if dataset_name in df['dataset'].unique():
+            df_dataset = df[df['dataset'] == dataset_name]
+            rmse_results = {}
+            algorithms = df_dataset['algoritmo'].unique()
+
+            for algorithm in algorithms:
+                df_algorithm = df_dataset[df_dataset['algoritmo'] == algorithm].sort_values(by='MV')
+                rmse_values = df_algorithm['metric1'].tolist()
+                rmse_results[algorithm] = rmse_values
+
+            datasets[dataset_name] = rmse_results
+        else:
+            print(f"Warning: Il dataset '{dataset_name}' non è presente nel file CSV.")
+
+    return datasets
+
+
+def plot_rmse_results(datasets, ncols=2):
+    num_datasets = len(datasets)
+    r1 = np.arange(10)  # Indici per le percentuali di valori mancanti
+
+    # Calcolo del numero di righe necessarie
+    nrows = (num_datasets + ncols - 1) // ncols  # Formula per arrotondare verso l'alto
+
+    #fig, axs = plt.subplots(nrows, ncols, figsize=(13, 6), sharey=True, sharex=True)
+    fig, axs = plt.subplots(nrows, ncols, figsize=(22, 8), sharey=True, sharex=True)
+    # Modifica degli spazi orizzontali e verticali
+    fig.subplots_adjust(hspace=0.20, wspace=0.04)  # Riduci hspace e wspace
+
+    # Appiattire gli assi se nrows > 1
+    axs = axs.flatten() if nrows > 1 else axs
+
+    for i, (dataset_name, results) in enumerate(datasets.items()):
+        # Plottare RMSE per ogni algoritmo in un singolo grafico
+        for algorithm, rmse_means in results.items():
+            print(algorithm, rmse_means)
+            if 'pipeline' in algorithm:
+                axs[i].plot(r1, rmse_means, marker="o", markersize=5, color="#ff0000", zorder=2, label='PipelineNoRev')
+            elif 'baseline' in algorithm:  # Altri algoritmi, se ce ne sono
+                axs[i].plot(r1, rmse_means, marker="x", markersize=8, color="#00748f", zorder=3, label='Pipeline')
+
+        axs[i].set_xticks(r1)
+        axs[i].set_xticklabels(["1", "2", "3", "4", "5", "10", "20", "30", "40", "50"])
+        axs[i].set_title(f'{dataset_name}', fontsize=10)
+        axs[i].tick_params(axis='x', which='major', labelsize=8)
+        axs[i].tick_params(axis='y', which='major', labelsize=11)
+        axs[i].minorticks_on()
+        axs[i].grid(which='major', linestyle='-', linewidth='0.5', color='gray', alpha=0.52)
+        axs[i].grid(which='minor', linewidth='0.5', linestyle='dotted', color='gray', alpha=0.4)
+        axs[i].xaxis.set_minor_locator(MultipleLocator(1))
+        axs[i].yaxis.set_minor_locator(MultipleLocator(0.05))
+        axs[i].set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1])
+
+        # Imposta l'etichetta RMSE solo per il primo subplot di ogni riga
+        if i % ncols == 0:
+            axs[i].set_ylabel('metric1', rotation='vertical')
+
+        # Imposta l'etichetta "Missing Rate" solo per l'ultima riga
+        if i >= (nrows - 1) * ncols:
+            axs[i].set_xlabel('Missing Rate')
+
+    # Gestire grafici vuoti se non ci sono abbastanza dataset
+    for j in range(i + 1, nrows * ncols):
+        axs[j].axis('off')  # Nascondere gli assi vuoti
+
+    handles = [
+        plt.Line2D([0], [0], color='#ff0000', linestyle='-', label='Pipeline', marker="o", markersize=10),
+        plt.Line2D([0], [0], color='#00748f', linestyle='-', label='Baseline', marker="x", markersize=12)
+    ]
+
+    fig.legend(handles=handles, loc='upper center', ncol=4, bbox_to_anchor=(0.5, 1), shadow=True)
+
+    plt.savefig("metric1_results.pdf", bbox_inches='tight')
+    plt.show()
+
+
+# Esempio di utilizzo
+filepath = '../ALL_Results_metric1.csv'  # Modifica con il percorso del tuo CSV
+selected_datasets = ['Boeing_898', 'actorfilms_4000', 'restaurant', 'NBA_3200', 'EV_Vehicles_4000', 'US_Presidents_3754', 'cars', "superstore_4500", "police", "IoT_Telemetry3000", "F1_REBUILT_5000", "MotoGP_REBUILT_3000", "Med_Ch_2500", "Air_9000", "restaurant_MNAR", "cars_MNAR", "Boeing_898_MNAR", "cars_MBUV", "Boeing_898_MBUV", "restaurant_MBUV"]  # Aggiungi altri dataset qui
+datasets = load_rmse_data(filepath, selected_datasets)  # Carica i dati RMSE
+print(datasets)
+plot_rmse_results(datasets, ncols=4)  # Mostra i risultati, specificando il numero di colonne
