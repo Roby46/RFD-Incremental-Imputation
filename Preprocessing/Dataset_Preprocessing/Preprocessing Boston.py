40db7c887a422ce39fdb2c807cf96d82203e8a6e
Roby46 <r.stanzione9@studenti.unisa.it>
2025-07-28 08:50:53 +0200

    Project Clean-Up

--- Preprocessing/Dataset_Preprocessing/Preprocessing Boston.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from unidecode import unidecode
-df=pd.read_csv("../../Datasets/Original_Datasets/crime.csv", sep=',', encoding='latin1')
-df.to_csv("../../Datasets/Preprocessed_Datasets/crimetemp.csv", sep=';', encoding='latin1')
-#df_winners=pd.read_csv("../../Datasets/Original_Datasets/winners.csv", sep=',', encoding='utf-8')
-print(df)
